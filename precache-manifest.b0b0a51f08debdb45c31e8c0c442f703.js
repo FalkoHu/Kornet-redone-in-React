e68658535ba9c4397893adb95cda19ce71e0513c
FalkoHu <35877916+FalkoHu@users.noreply.github.com>
2019-02-13 23:53:36 -0600

    Updates

---
+++ precache-manifest.b0b0a51f08debdb45c31e8c0c442f703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f25077d264a18cfb935",
+    "url": "/RestaurantAPI/static/js/runtime~main.5f25077d.js"
+  },
+  {
+    "revision": "bd1548de359fcb05b9f8",
+    "url": "/RestaurantAPI/static/js/main.bd1548de.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/RestaurantAPI/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "bd1548de359fcb05b9f8",
+    "url": "/RestaurantAPI/static/css/main.5077b8fa.chunk.css"
+  },
+  {
+    "revision": "c16f7ba7c568d301ef2c1bef3d061d77",
+    "url": "/RestaurantAPI/index.html"
+  }
+];
